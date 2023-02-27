95bd99fca73cea40c6581b53a08df4a658d78005
DomenicoLucarelli <domyluca@gmail.com>
2023-02-27 22:38:43 +0100

    fix array

--- script.js
+++ script.js
@@ -61,59 +61,14 @@ logEl.addEventListener("click" , function() {
 
 
 
-// let generatorEl = document.getElementById("generator");
-
-// generatorEl.addEventListener("click" , function(){
-
-//     let userNumber = Math.floor(Math.random() * 6 + 1);
-    
-//     let computerNumber = Math.floor(Math.random() * 6 + 1);
-
-//     let userImageEl = document.getElementById("user-image");
-
-//     userImageEl.innerHTML =(userNumber);
-
-//     let computerImageEl = document.getElementById("computer-image");
-
-//     computerImageEl.innerHTML =(computerNumber);
-
-//     let resultEl = document.getElementById("result");
-    
-    
-
-//     if(userNumber > computerNumber){
-
-//         resultEl.innerHTML=("Hai vinto")
-        
-
-//     }else if(userNumber < computerNumber){
-
-//         resultEl.innerHTML=("Hai perso")
-
-
-//     }else{
-
-//         resultEl.innerHTML=("Pareggio: ritenta la fortuna")
-
-
-//     }
-
-// })
-
 let diceFaces = []
 
-diceFaces[0] = new Image();
-diceFaces[0].src = 'img/dice-one-solid.svg';
-diceFaces[1] = new Image();
-diceFaces[1].src = 'img/dice-two-solid.svg';
-diceFaces[2] = new Image();
-diceFaces[2].src = 'img/dice-three-solid.svg';
-diceFaces[3] = new Image();
-diceFaces[3].src = 'img/dice-four-solid.svg';
-diceFaces[4] = new Image();
-diceFaces[4].src = 'img/dice-five-solid.svg';
-diceFaces[5] = new Image();
-diceFaces[5].src = 'img/dice-six-solid.svg';
+diceFaces[0]  = 'img/dice-one-solid.svg';
+diceFaces[1]  = 'img/dice-two-solid.svg';
+diceFaces[2]  = 'img/dice-three-solid.svg';
+diceFaces[3]  = 'img/dice-four-solid.svg';
+diceFaces[4]  = 'img/dice-five-solid.svg';
+diceFaces[5]  = 'img/dice-six-solid.svg';
 
 let generatorEl = document.getElementById("generator");
 
@@ -121,20 +76,31 @@ let userImageEl = document.getElementById("user-image");
 
 let computerImageEl = document.getElementById("computer-image");
 
+let resultEl = document.getElementById("result");
+
 let userNumber
+
 let computerNumber
+
+let computerImage = new Image()
+
+let userImage = new Image()
 
 generatorEl.addEventListener("click" , function(){
 
     userNumber = Math.floor(Math.random() * 6  );
+
+    userImage.src = diceFaces[userNumber]
     
     computerNumber = Math.floor(Math.random() * 6 );
 
-    userImageEl.appendChild(diceFaces[userNumber]);
+    computerImage.src = diceFaces[computerNumber]
 
-    computerImageEl.appendChild(diceFaces[computerNumber]);
+    userImageEl.appendChild(userImage);
+
+    computerImageEl.appendChild(computerImage);
     
-    let resultEl = document.getElementById("result");
+    
 
     if(userNumber > computerNumber){
 
@@ -163,8 +129,10 @@ let resetEl = document.getElementById("reset");
 
 resetEl.addEventListener("click" , function(){
 
-    diceFaces[userNumber].remove() 
-    diceFaces[computerNumber].remove()
+   userImage.remove() 
+   computerImage.remove()
+   resultEl.innerHTML = ""
+   
 
 
 })
