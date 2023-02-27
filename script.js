2d66bbd8eedb00512425bdfa0218adb57a52756f
DomenicoLucarelli <domyluca@gmail.com>
2023-02-27 20:58:45 +0100

    esercizio terminato con immagini

--- script.js
+++ script.js
@@ -61,49 +61,98 @@ logEl.addEventListener("click" , function() {
 
 
 
+// let generatorEl = document.getElementById("generator");
+
+// generatorEl.addEventListener("click" , function(){
+
+//     let userNumber = Math.floor(Math.random() * 6 + 1);
+    
+//     let computerNumber = Math.floor(Math.random() * 6 + 1);
+
+//     let userImageEl = document.getElementById("user-image");
+
+//     userImageEl.innerHTML =(userNumber);
+
+//     let computerImageEl = document.getElementById("computer-image");
+
+//     computerImageEl.innerHTML =(computerNumber);
+
+//     let resultEl = document.getElementById("result");
+    
+    
+
+//     if(userNumber > computerNumber){
+
+//         resultEl.innerHTML=("Hai vinto")
+        
+
+//     }else if(userNumber < computerNumber){
+
+//         resultEl.innerHTML=("Hai perso")
+
+
+//     }else{
+
+//         resultEl.innerHTML=("Pareggio: ritenta la fortuna")
+
+
+//     }
+
+// })
+
+let diceFaces = []
+
+diceFaces[0] = new Image();
+diceFaces[0].src = 'img/dice-one-solid.svg';
+diceFaces[1] = new Image();
+diceFaces[1].src = 'img/dice-two-solid.svg';
+diceFaces[2] = new Image();
+diceFaces[2].src = 'img/dice-three-solid.svg';
+diceFaces[3] = new Image();
+diceFaces[3].src = 'img/dice-four-solid.svg';
+diceFaces[4] = new Image();
+diceFaces[4].src = 'img/dice-five-solid.svg';
+diceFaces[5] = new Image();
+diceFaces[5].src = 'img/dice-six-solid.svg';
+
 let generatorEl = document.getElementById("generator");
 
 generatorEl.addEventListener("click" , function(){
 
-    let userNumber = Math.floor(Math.random() * 6 + 1);
+    let userNumber = Math.floor(Math.random() * 6  );
     
-    let computerNumber = Math.floor(Math.random() * 6 + 1);
+    let computerNumber = Math.floor(Math.random() * 6 );
 
     let userImageEl = document.getElementById("user-image");
 
-    userImageEl.innerHTML =(userNumber);
+    userImageEl.appendChild(diceFaces[userNumber])
+    
 
     let computerImageEl = document.getElementById("computer-image");
 
-    computerImageEl.innerHTML =(computerNumber);
+    computerImageEl.appendChild(diceFaces[computerNumber])
+    
 
     let resultEl = document.getElementById("result");
-    
-    
 
     if(userNumber > computerNumber){
 
-        resultEl.innerHTML=("Hai vinto")
+                resultEl.innerHTML=("Hai vinto")
+                
         
-
-    }else if(userNumber < computerNumber){
-
-        resultEl.innerHTML=("Hai perso")
-
-
-    }else{
-
-        resultEl.innerHTML=("Pareggio: ritenta la fortuna")
-
-
-    }
-
-})
-    
+            }else if(userNumber < computerNumber){
         
+                resultEl.innerHTML=("Hai perso")
+        
+        
+            }else{
+        
+                resultEl.innerHTML=("Pareggio: ritenta la fortuna")
+        
+        
+            }
 
-    
-
+})       
 
 
 
